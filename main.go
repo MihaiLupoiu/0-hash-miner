e6767da687b530d60885c7ea35c70e110d4c85b0
mihai <myhay.360@gmail.com>
2021-07-30 16:05:24 +0200

    Very basic working version.

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"crypto/tls"
+	"encoding/hex"
 	"flag"
-	"io"
+	"fmt"
 	"log"
-	"os"
+	"net/textproto"
+	"strconv"
 	"strings"
-	"sync"
+
+	"crypto/sha1"
+
+	"github.com/google/uuid"
 )
 
 func createClientConfig(crt, key string) (*tls.Config, error) {
@@ -42,6 +48,22 @@ func printConnState(conn *tls.Conn) {
 	log.Print(">>>>>>>>>>>>>>>> State End <<<<<<<<<<<<<<<<")
 }
 
+var authdata = ""
+
+func hexStartsWith(hash [20]byte, dificulty int) bool {
+	// Improve method to use bit manipulation for more optimal comparion.
+	sha1_hash := hex.EncodeToString(hash[:])
+	fmt.Println(hash, sha1_hash)
+
+	prefixDifficulty := strings.Repeat("0", dificulty)
+	fmt.Println(prefixDifficulty)
+
+	res := strings.HasPrefix(sha1_hash, prefixDifficulty)
+	fmt.Println("Result:", res)
+
+	return res
+}
+
 func main() {
 	connect := flag.String("connect", "localhost:4433", "who to connect to")
 	crt := flag.String("crt", "./configs/certs/public.crt", "certificate")
@@ -67,17 +89,108 @@ func main() {
 	log.Printf("connect to %s succeed", addr)
 	printConnState(conn)
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		io.Copy(conn, os.Stdin)
-		wg.Done()
-	}()
+	reader := bufio.NewReader(conn)
+	tp := textproto.NewReader(reader)
 
-	wg.Add(1)
-	go func() {
-		io.Copy(os.Stdout, conn)
-		wg.Done()
-	}()
-	wg.Wait()
+	// defer conn.Close()
+
+	for {
+		// read one line (ended with \n or \r\n)
+		line, _ := tp.ReadLine()
+		fmt.Println(line)
+
+		switch line {
+		case "HELO":
+			conn.Write([]byte("EHLO\n"))
+
+		case "END":
+			// if you get this command, then your data was submitted
+			conn.Write([]byte("OK\n"))
+
+		// the rest of the data server requests are required to identify you
+		// and get basic contact information
+		case "NAME":
+			// as the response to the NAME request you should send your full name
+			// including first and last name separated by single space
+			// conn.write(SHA1(authdata+args[1]) + " " + "My name\n")
+
+		case "MAILNUM":
+			// here you specify, how many email addresses you want to send
+			// each email is asked separately up to the number specified in MAILNUM
+			// conn.write(SHA1(authdata+args[1]) + " " + "2\n")
+
+		case "MAIL1":
+			// conn.write(SHA1(authdata+args[1]) + " " + "my.name@example.com\n")
+
+		case "MAIL2":
+			// conn.write(SHA1(authdata+args[1]) + " " + "my.name2@example.com\n")
+
+		case "SKYPE":
+			// here please specify your Skype account for the interview, or N/A
+			// in case you have no Skype account
+			// conn.write(SHA1(authdata+args[1]) + " " + "my.name@example.com\n")
+
+		case "BIRTHDATE":
+			// here please specify your birthdate in the format %d.%m.%Y
+			// conn.write(SHA1(authdata+args[1]) + " " + "01.02.2017\n")
+
+		case "COUNTRY":
+			// country where you currently live and where the specified address is
+			// please use only the names from this web site:
+			//   https://www.countries-ofthe-world.com/all-countries.html
+			// conn.write(SHA1(authdata+args[1]) + " " + "Germany\n")
+
+		case "ADDRNUM":
+			// specifies how many lines your address has, this address should
+			// be in the specified country
+			// conn.write(SHA1(authdata+args[1]) + " " + "2\n")
+
+		case "ADDRLINE1":
+			// conn.write(SHA1(authdata+args[1]) + " " + "Long street 3\n")
+
+		case "ADDRLINE2":
+			// conn.write(SHA1(authdata+args[1]) + " " + "32345 Big city\n")
+
+		default:
+			if strings.HasPrefix(line, "POW") {
+				fmt.Println(line)
+				fmt.Println("Time to work")
+				args := strings.Fields(line)
+
+				authdata = args[1]
+				difficulty, err := strconv.Atoi(args[2])
+				if err != nil {
+					log.Fatalf("Difficulty of POW not integer: %s", err.Error())
+				}
+
+				fmt.Println(authdata, difficulty)
+
+				for {
+
+					// generate short random string, server accepts all utf-8 characters,
+					// except [\n\r\t ], it means that the suffix should not contain the
+					// characters: newline, carriege return, tab and space
+					suffix := uuid.New().String()
+					fmt.Printf("String: %s\n", suffix)
+
+					cksum_in_hex := sha1.Sum([]byte(authdata + suffix))
+					fmt.Printf("  SHA1: %x\n", cksum_in_hex)
+
+					// check if the checksum has enough leading zeros
+					// (length of leading zeros should be equal to the difficulty)
+					if hexStartsWith(cksum_in_hex, difficulty) {
+						conn.Write([]byte(suffix + "\n"))
+						break
+					}
+
+				}
+			} else if strings.HasPrefix(line, "ERROR") {
+				fmt.Println(line)
+				break
+			}
+		}
+
+		// do something with data here, concat, handle and etc...
+	}
+
 }
